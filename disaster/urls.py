4b29254b5858da11aa743e918c11cdca4696ffb1
Morshed07 <inayeem729@gmail.com>
2026-07-10 18:00:55 +0600

    update static path in urls

--- disaster/urls.py
+++ disaster/urls.py
@@ -26,4 +26,7 @@ urlpatterns = [
     path('api/v1/', include("apps.api.urls"))
 
 
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
